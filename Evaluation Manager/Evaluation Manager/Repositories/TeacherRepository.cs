844ba89cf6f0352424b3a4c92dc8d06e21e22444
Keyraus <c.m2002@hotmail.fr>
2023-05-16 10:57:47 +0200

    Create TeacherRepository

---
+++ Evaluation Manager/Evaluation Manager/Repositories/TeacherRepository.cs
@@ -0,0 +1,48 @@
+﻿using DBLayer;
+using Evaluation_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Repositories
+{
+    public static class TeacherRepository
+    {
+        public static Teacher GetTeacher(int id)
+        {
+            Teacher teacher = null;
+            string sql = $"SELECT * FROM Teacher WHERE Id = {id}";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                teacher = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return teacher;
+        }
+
+        private static Teacher CreateObject(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["Id"].ToString());
+            string firstName = reader["FirstName"].ToString();
+            string lastName = reader["LastName"].ToString();
+            string username = reader["Username"].ToString();
+            string password = reader["Password"].ToString();
+            var teacher = new Teacher
+            {
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName,
+                Username = username,
+                Password = password
+            };
+            return teacher;
+        }
+    }
+}
